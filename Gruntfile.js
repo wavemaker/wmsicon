397c14ec5b7ae9e2f320aec188d5a68a3d998150
sampathg9 <sampath.gembali@wavemaker.com>
2018-10-12 11:01:49 +0530

    Refactored WMS icons

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,67 +1,34 @@
-/*jshint node:true*/
-
 var path = require('path');
 
 module.exports = function (grunt) {
 	'use strict';
 
-	require('load-grunt-tasks')(grunt);
+	grunt.loadNpmTasks('grunt-webfont');
 
 	grunt.initConfig({
 		webfont: {
 			wmsicon: {
 				src: 'src/**/*.svg',
-				dest: 'dist/wmsicon',
+				dest: 'dist/wmsicon/fonts',
+				destCss: 'dist/wmsicon/css',
 				options: {
 					hashes: false,
-					dest: 'dist/wmsicon/fonts',
-					destCss: 'dist/wmsicon/css',
 					fontFamilyName: 'wms',
+					templateOptions: {
+						baseClass: "wms",
+						classPrefix: "wms-",
+						mixinPrefix: "wms-"
+					},
+					htmlDemoTemplate: 'templates/demo.html',
 					font: 'wmsicon'
 				}
 			}
-		},
-		// svgstore: {
-		// 	options: {
-		// 		prefix: 'wms-',
-		// 		svg: {
-		// 			viewBox: '0 0 100 100',
-		// 			xmlns: 'http://www.w3.org/2000/svg'
-		// 		}
-		// 	},
-		// 	default: {
-		// 		files: {
-		// 			'dist/wmsicon/proavatar.svg': ['src/project-icons/*.svg'],
-		// 		},
-		// 	},
-		// },
-		jshint: {
-			all: ['Gruntfile.js', 'tasks/*.js', 'test/*.js'],
-			options: {
-				jshintrc: true
-			}
-		},
-		watch: {
-			scripts: {
-				files: '<%= jshint.all %>',
-				tasks: ['jshint', 'jscs'],
-				options: {
-					debounceDelay: 100,
-					nospawn: true
-				}
-			}
-		},
-		jscs: {
-			options: {
-				config: ".jscs.json"
-			},
-			all: ['tasks/*.js']
 		},
 		clean: ['test/tmp']
 	});
 
 	grunt.loadTasks('tasks');
 
-	grunt.registerTask('default', ['jshint', 'jscs', 'webfont', /*'svgstore',*/ 'clean']);
+	grunt.registerTask('default', ['webfont'/*, 'clean'*/]);
 
 };
